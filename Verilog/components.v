a5d3b6dc1e2d23bed4bdbb813425250b52fe23d9
مُحَمَّــد أبو شِلبايَة <mabushelbaia@gmail.com>
2023-03-06 12:56:19 +0200

    Finished Memories

--- Verilog/components.v
+++ Verilog/components.v
@@ -22,3 +22,33 @@ module regFile(input [2:0] rs, rt, rd, input [23:0] dataIn, output reg [23:0] da
         end
     end
 endmodule
+
+
+module data_mem(
+    input [23:0] address,
+    input [23:0] dataIn,
+    input clk, we, re,
+    output reg [23:0] dataOut
+    );
+    reg [7:0] memory [2**24-1:0];
+    always @(posedge clk) begin
+        if (we) {memory[address+2], memory[address+1], memory[address]} <= {dataIn};
+        if (re) {dataOut} <= {memory[address+2], memory[address+1], memory[address]};
+    end					
+endmodule
+module instruction_mem(
+    input [23:0] address, 
+    input clk, 
+    input en,
+    output reg [23:0] instruction
+    );
+    reg [7:0] memory [2**24-1:0];
+    assign {memory[2],memory[1],memory[0]} = 24'hDEADBE;
+    assign {memory[5],memory[4],memory[3]} = 24'hFEEDBA;
+    assign {memory[8],memory[7],memory[6]} = 24'hABCDEF;
+    assign {memory[11],memory[10],memory[9]} = 24'hC0FFEE;
+    assign {memory[14],memory[13],memory[12]} = 24'hBEEFCA;
+    always @(posedge clk) begin
+        if (en) instruction <= {memory[address+2], memory[address+1], memory[address]};
+    end
+endmodule
--- Verilog/testbenches.v
+++ Verilog/testbenches.v
@@ -26,3 +26,78 @@ module regFile_test;
         we = 0;
     end
 endmodule
+
+
+module instruction_mem_test;
+    reg clk, instruction_en;
+    reg [23:0] pc, instruction;
+    initial begin 
+		clk = 1;
+        #100 $finish;
+    end
+    always #5 clk = ~clk;
+    instruction_mem im (pc, clk, instruction_en, instruction);
+    always @(negedge clk) begin	  
+        $display("pc: 0x%h, instruction: 0x%h", pc, instruction);
+    end
+    initial begin
+        instruction_en = 1;
+        pc = 0;
+        #10
+        pc = 3;
+        #10
+        pc = 6;
+        #10
+        instruction_en = 0;
+        #10
+        pc = 9;
+    end
+endmodule
+module data_mem_test;
+    reg [23:0] address;
+    reg [23:0] dataIn;
+    reg clk, we, re;
+    wire [23:0] dataOut;
+    data_mem dm (address, dataIn, clk, we, re, dataOut);
+    initial begin 
+        clk = 0;
+        #100 $finish;
+    end
+    always #5 clk = ~clk;
+    always @(negedge clk) begin
+        $display("address: %b, dataIn: %b, clk: %b, we: %b, re: %b, dataOut: %b", address, dataIn, clk, we, re, dataOut);
+    end
+    initial begin
+        address = 0;
+        dataIn = 100;
+        we = 1;
+        re = 0;
+        #10
+        address = 3;
+        dataIn = 200;
+        #10
+        address = 6;
+        dataIn = 300;
+        #10
+        address = 9;
+        dataIn = 0;
+        we = 1;
+        #10
+        address = 0;
+        re = 1;
+        #10
+        address = 3;
+        re = 1;
+        #10
+        address = 6;
+        re = 1;
+        #10
+        address = 9;
+        re = 1;
+        #10
+        address = 0;
+        re = 1;	 
+		#10
+		$finish;
+    end
+endmodule
